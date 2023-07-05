b51e0901034b58574f3e5f193046ae85c8f4beb8
Thiên. Nguyễn Hoàng (6) <thiennh6@vng.com.vn>
2023-07-05 15:54:10 +0700

    Update check iOS version

--- src/app/discord/bot.ts
+++ src/app/discord/bot.ts
@@ -26,28 +26,23 @@ client.once("ready", () => {
     console.error(`Channel with ID '${CHANNEL_ID}' not found.`);
     return;
   }
-  termRepo.removeApks().then((isRemove) => {
-    if (isRemove) {
-      // getAppInTarget.execute().then(console.log).catch(console.log);
-      getAppInTarget.executeIOSTarget().then(console.log).catch(console.log);
-    }
-  });
 
-  // let job = new CronJob(
-  //   "* * * * *",
-  //   async function () {
-  //     const isRemoveApks = await termRepo.removeApks();
-  //     if (isRemoveApks) {
-  //       await getAppInTarget.execute();
-  //     } else {
-  //       console.log("Disk full !");
-  //     }
-  //   },
-  //   null,
-  //   true,
-  //   "Asia/Ho_Chi_Minh"
-  // );
-  // job.start();
+  let job = new CronJob(
+    "0 8 * * *",
+    async function () {
+      const isRemoveApks = await termRepo.removeApks();
+      if (isRemoveApks) {
+        await getAppInTarget.executeIOSTarget();
+        await getAppInTarget.execute();
+      } else {
+        console.log("Disk full !");
+      }
+    },
+    null,
+    true,
+    "Asia/Ho_Chi_Minh"
+  );
+  job.start();
 });
 
 client.on("interactionCreate", async (interaction) => {
